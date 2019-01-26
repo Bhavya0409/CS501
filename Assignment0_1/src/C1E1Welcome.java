ff349b1174d2151e5400999b5fdf4f6302c2b2b7
DESKTOP-91C766U\bhavy <bhavya0409@gmail.com>
2019-01-26 18:19:40 -0500

    moved the text into system out

--- Assignment0_1/src/C1E1Welcome.java
+++ Assignment0_1/src/C1E1Welcome.java
@@ -1,15 +1,15 @@
 /**
  *
  * @author Jonathan Fishkin, Bhavya Shah
- * This program will display 3 messages 
  * I pledge that I have abided by the Stevens Honor System
  */
 public class C1E1Welcome {
-    
+
     public static void main(String[] args){
+        System.out.println("This is a program to output 3 lines of code into the command line.");
         System.out.println("Welcome to Java");
         System.out.println("Learning Java Now");
         System.out.println("Programming is fun");
     }
-    
+
 }
