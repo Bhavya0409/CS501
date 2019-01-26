01344ea06b4dd89b0b3dcc89386861d82b716d3c
DESKTOP-91C766U\bhavy <bhavya0409@gmail.com>
2019-01-26 18:22:15 -0500

    moved helper text into system.out statement

--- Assignment0_1/src/C1E12AverageSpeed.java
+++ Assignment0_1/src/C1E12AverageSpeed.java
@@ -1,12 +1,12 @@
 /**
  *
  * @author Jonathan Fishkin, Bhavya Shah
- * This program will display average speed in kilometers per hour
  * I pledge that I have abided by the Stevens Honor System
  */
 public class C1E12AverageSpeed {
-    
+
     public static void main(String[] args){
+        System.out.println("This program will calculate the speed of a runner in km/h given miles ran in a certain time.");
         double milesToKilometer = (24 * 1.6);
         double totalHours = (40.00 / 60.00) + (35.00 / 3600.00) + 1.0;
         double averageSpeed = milesToKilometer / totalHours;
