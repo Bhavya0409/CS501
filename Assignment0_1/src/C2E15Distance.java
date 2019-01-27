c2f5d94aae63e292d4b802ba3344b8a23369be69
DESKTOP-91C766U\bhavy <bhavya0409@gmail.com>
2019-01-26 19:37:07 -0500

    changed file to accept characters space separated

--- Assignment0_1/src/C2E15Distance.java
+++ Assignment0_1/src/C2E15Distance.java
@@ -1,25 +1,31 @@
 /**
  *
  * @author Jonathan Fishkin, Bhavya Shah
- * This program will display the distance between two points
  * I pledge that I have abided by the Stevens Honor System
  */
 import java.util.Scanner;
 public class C2E15Distance {
     public static void main(String[] args){
+        System.out.println("This program will calculate the distance between the two points.");
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter x1 and y1");
-        double x1 = input.nextDouble();
-        System.out.println("Your value for x1 is:" + x1);
-        double y1 = input.nextDouble();
-        System.out.println("Your value for y1 is:" + y1);
-        System.out.println("Please enter x2 and y2");
-        double x2 = input.nextDouble();
-        System.out.println("Your value for x1 is:" + x2);
-        double y2 = input.nextDouble();
-        System.out.println("Your value for y1 is:" + y2);
+
+        System.out.println("Please enter x1 and y1, separated by spaces");
+        String first = input.nextLine();
+        String[] firstCoordinates = first.split(" ");
+        double x1 = Double.parseDouble(firstCoordinates[0]);
+        double y1 = Double.parseDouble(firstCoordinates[1]);
+        System.out.println("your x1 value is: " + x1);
+        System.out.println("your y1 value is: " + y1);
+
+        System.out.println("Please enter x2 and y2, separated by spaces");
+        String second = input.nextLine();
+        String[] secondCoordinates = second.split(" ");
+        double x2 = Double.parseDouble(secondCoordinates[0]);
+        double y2 = Double.parseDouble(secondCoordinates[1]);
+        System.out.println("your x1 value is: " + x2);
+        System.out.println("your y1 value is: " + y2);
         input.close();
-        
+
         double allPoints = Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2);
         double distance = Math.pow(allPoints, 0.5);
         System.out.println("The distance between two points is: "  + distance);
