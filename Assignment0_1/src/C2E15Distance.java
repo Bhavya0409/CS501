ffc89ecb02641ed62f5ae6040e12281b0101a179
Thefish1996 <rfishkin1313@gmail.com>
2019-01-27 17:16:44 -0500

    update to 1 assignment

--- Assignment0_1/src/C2E15Distance.java
+++ Assignment0_1/src/C2E15Distance.java
@@ -9,13 +9,13 @@ public class C2E15Distance {
         System.out.println("This program will calculate the distance between the two points.");
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter x1 and y1, separated by spaces");
+        System.out.print("Please enter x1 and y1, separated by spaces ");
         double x1 = input.nextDouble();
         double y1 = input.nextDouble();
         System.out.println("your x1 value is: " + x1);
         System.out.println("your y1 value is: " + y1);
 
-        System.out.println("Please enter x2 and y2, separated by spaces");
+        System.out.print("Please enter x2 and y2, separated by spaces ");
         double x2 = input.nextDouble();
         double y2 = input.nextDouble();
         System.out.println("your x1 value is: " + x2);
