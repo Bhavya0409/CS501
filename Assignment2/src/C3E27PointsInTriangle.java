91441cb423347b00c2acca7b667df256493e6b75
Jon <rfishkin1313@gmail.com>
2019-01-28 16:24:31 -0500

    Assignment2

---
+++ Assignment2/src/C3E27PointsInTriangle.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Jonathan Fishkin, Bhavya Shah
+ * I pledge that I have abided by the Stevens Honor System
+ */
+import java.util.Scanner;
+public class C3E27PointsInTriangle {
+    public static void main(String[] args){
+        System.out.println("This program determines if a set of points are in a triangle");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a point's x- and y-coordinates seperated by a space ");
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+        
+        double xIntersect = (-x * (200 * 100)) / (-y * 200 - x * 100);
+        double yIntersect = (-y * (200 * 100)) / (-y * 200 - x * 100);
+        
+        if (x > xIntersect || y > yIntersect){
+            System.out.println("The point is not in the triangle");    
+        } else {
+            System.out.println("The point is in the triangle"); 
+        }
+        
+    }
+}
---
+++ Assignment2/src/C3E3LinearEquations.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author Jonathan Fishkin, Bhavya Shah
+ * I pledge that I have abided by the Stevens Honor System
+ */
+import java.util.Scanner;
+public class C3E3LinearEquations {
+    public static void main(String[] args){
+        System.out.println("This program prompts the user to enter a, b, c, d, e, and f and displays the result to this linear equation");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a, b, c, d, e, f points seperated by spaces: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double d = input.nextDouble();
+        double e = input.nextDouble();
+        double f = input.nextDouble();
+        System.out.println("The value of a is: " + a);
+        System.out.println("The value of a is: " + b);
+        System.out.println("The value of a is: " + c);
+        System.out.println("The value of a is: " + d);
+        System.out.println("The value of a is: " + e);
+        System.out.println("The value of a is: " + f);
+        System.out.println("The resulting first equation would be " + a + "x + " + b + "y = " + e);
+        System.out.println("The resulting second equation would be " + c + "x + " + d + "y = " + f);
+        
+        
+        if((a*d) - (b*c) == 0) {
+           System.out.println("The equation has no solution");
+        } else {
+           double x = (e*d - b*f)/(a*d - b*c);
+           double y = (a*f - e*c)/(a*d - b*c);
+           System.out.println("The value of x is: " + x + " and y is: " + y);
+        }     
+    }
+}
