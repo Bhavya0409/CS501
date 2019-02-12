41022d49d94f9a3a97cea2ad83c7ae2ed66f8d3c
Thefish1996 <rfishkin1313@gmail.com>
2019-02-12 16:27:16 -0500

    formatting

--- Assignment3/C7E91020ArrayElements.java
+++ Assignment3/C7E91020ArrayElements.java
@@ -30,21 +30,21 @@ public class C7E91020ArrayElements {
     public static void main(String[] args){
         System.out.println("This program will search for the maximum value and index of an array, and sort it in ascending order.");
         Scanner input = new Scanner(System.in);
-				while (true) {
-					System.out.println("Please enter 10 numbers, separated by spaces. You can quit the program at any time by having -1 as your first number.");
-					double first = input.nextDouble();
-					if (first == -1.0) {
-						break;
-					}
-					double[] numbers = new double[10];
-					numbers[0] = first;
-					for (int i = 1; i < 10; i++) {
-						numbers[i] = input.nextDouble();
-					}
-					System.out.println("Your numbers are: " + Arrays.toString(numbers));
-					System.out.println("Your maximum number is " + max(numbers) + " and its index is: " + indexOfLargestElement(numbers));
+			while (true) {
+				System.out.println("Please enter 10 numbers, separated by spaces. You can quit the program at any time by having -1 as your first number.");
+				double first = input.nextDouble();
+				if (first == -1.0) {
+					break;
 				}
-				System.out.println("Program exit.");
+				double[] numbers = new double[10];
+				numbers[0] = first;
+				for (int i = 1; i < 10; i++) {
+					numbers[i] = input.nextDouble();
+				}
+				System.out.println("Your numbers are: " + Arrays.toString(numbers));
+				System.out.println("Your maximum number is " + max(numbers) + " and its index is: " + indexOfLargestElement(numbers));
+			}
+			System.out.println("Program exit.");
 		input.close();
     }
 }
