f49e8cc4f258c1869035945b23cbebd00be9182a
Thefish1996 <rfishkin1313@gmail.com>
2019-02-25 10:14:08 -0500

    c9e5 added

--- Assignment4/C9E5Gregorian.java
+++ Assignment4/C9E5Gregorian.java
@@ -11,11 +11,22 @@ public class C9E5Gregorian {
         System.out.println("This program is to test the Gregorian API");
 
         GregorianCalendar newCalendar = new GregorianCalendar();
-    }
+        String currentDate = Current(newCalendar);
+        System.out.println(currentDate);
+        String specificDate = specificDate(newCalendar);
+        System.out.println(specificDate);
 
+    }
     public static String Current (GregorianCalendar calendar){
-        System.out.println("Current year, month, and day in format Mth/Day/Year: ");
-        String currentDate = calendar.get(calendar.MONTH) + 
-        return "Hello";
+        System.out.print("Current year, month, and day in format Month/Day/Year: ");
+        int Month = calendar.get(calendar.MONTH) + 1;
+        String currentDate = Month + "/" + calendar.get(calendar.DAY_OF_MONTH) + "/"  + calendar.get(calendar.YEAR);
+        return currentDate;
+    }
+    public static String specificDate (GregorianCalendar calendar) {
+        System.out.print("Elapsed time since January 1, 1970 set to 1234567898765L in format Month/Day/Year: ");
+        calendar.setTimeInMillis(1234567898765L);
+        String currentDate = calendar.get(calendar.MONTH) + "/" + calendar.get(calendar.DAY_OF_MONTH) + "/"  + calendar.get(calendar.YEAR);
+        return currentDate;
     }
 }
